7a38398313cb9010820bfac1970717b76b02f6e1
Retdyel <77490836+Retdyel@users.noreply.github.com>
2022-07-05 08:03:47 +0100

    Update models.py

--- I4G019308PUI/links/models.py
+++ I4G019308PUI/links/models.py
@@ -6,7 +6,7 @@ class Link(models.Model):
     target_url = models.URLField(max_length=200)
     description = models.CharField(max_length=200)
     identifier = models.SlugField(blank=True, unique=True, max_length=20)
-    author = get_user_model()
-    created_date = models.DateTimeField
+    author = models.ForeignKey(get_user_model())
+    created_date = models.DateTimeField()
     active = models.BooleanField(default=True)
     
